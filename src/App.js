fff0d21f3dcce5c13bc93af2bfbe2b607358f4ef
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-03-08 01:33:51 -0300

    requisito 2 finalizado finalmente

--- src/App.js
+++ src/App.js
@@ -7,33 +7,21 @@ import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
+import Carregando from './pages/Carregando';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact>
-            <Login />
-          </Route>
-          <Route path="/search" exact>
-            <Search />
-          </Route>
-          <Route path="/album/:id" exact>
-            <Album />
-          </Route>
-          <Route path="/favorites" exact>
-            <Favorites />
-          </Route>
-          <Route path="/profile" exact>
-            <Profile />
-          </Route>
-          <Route path="/profile/edit" exact>
-            <ProfileEdit />
-          </Route>
-          <Route path="*" exact>
-            <NotFound />
-          </Route>
+          <Route path="/" exact component={ Login } />
+          <Route path="/search" exact component={ Search } />
+          <Route path="/album/:id" exact component={ Album } />
+          <Route path="/favorites" exact component={ Favorites } />
+          <Route path="/profile" exact component={ Profile } />
+          <Route path="/profile/edit" exact component={ ProfileEdit } />
+          <Route path="/carregando" exact component={ Carregando } />
+          <Route path="*" exact component={ NotFound } />
         </Switch>
       </BrowserRouter>
     );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,67 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Login extends React.Component {
-  render() {
-    return (
-      <div data-testid="page-login">
-        <input type="text" data-testid="login-name-input" />
-        <button data-testid="login-submit-button" type="submit">Entrar</button>
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      carregar: false,
+      redirecionar: false,
+      nameU: '',
+      buttonDisable: true,
+    };
   }
+
+habilitedButton = ({ target }) => {
+  const nameNum = target.value;
+  const minNum = 3;
+  if (nameNum.length >= minNum) {
+    this.setState({
+      buttonDisable: false,
+      nameU: target.value,
+    });
+  } else {
+    this.setState({
+      buttonDisable: true,
+    });
+  }
+}
+
+clickButton = async () => {
+  const { state } = this;
+  this.setState({ carregar: true });
+  await createUser({ name: state.nameU });
+  this.setState({ carregar: false, redirecionar: true });
+};
+
+render() {
+  const { state } = this;
+  return (
+    <div data-testid="page-login">
+      { state.carregar ? <Carregando />
+        : (
+          <>
+            <input
+              onChange={ this.habilitedButton }
+              type="text"
+              data-testid="login-name-input"
+            />
+            <button
+              disabled={ state.buttonDisable }
+              data-testid="login-submit-button"
+              onClick={ this.clickButton }
+              type="submit"
+            >
+              Entrar
+            </button>
+            { state.redirecionar && <Redirect to="/search" /> }
+          </>
+        )}
+    </div>
+  );
+}
 }
 
 export default Login;
