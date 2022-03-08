e67a0826f000f05d7f76f198d2448fcf842c85ee
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-03-08 02:54:12 -0300

    requisito 5 finalizado

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,46 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      habilitedBtn: true,
+    };
+  }
+
+  btnHabi = ({ target }) => {
+    const minNum = 2;
+    if (target.value.length >= minNum) {
+      this.setState({
+        habilitedBtn: false,
+      });
+    } else {
+      this.setState({
+        habilitedBtn: true,
+      });
+    }
+  };
+
   render() {
+    const { state } = this;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            onChange={ this.btnHabi }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="submit"
+            disabled={ state.habilitedBtn }
+          >
+            Pesquisar
+
+          </button>
+        </form>
       </div>
     );
   }
