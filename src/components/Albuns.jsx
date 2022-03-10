9d62d2a0713ad36e46de777fb0782e775d340d0f
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-03-09 22:39:32 -0300

    finalmente requisito 6

---
+++ src/components/Albuns.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class Albuns extends React.Component {
+  render() {
+    const { collectionId, imageThum, nameArt, nameAlb } = this.props;
+    return (
+      <Link to={ `/album/${collectionId}` }>
+        <div data-testid={ `link-to-album-${collectionId}` }>
+
+          <img src={ imageThum } alt={ nameAlb } />
+          <h3>{ nameAlb }</h3>
+          <p>{ nameArt }</p>
+        </div>
+      </Link>
+    );
+  }
+}
+
+Albuns.propTypes = {
+  collectionId: PropTypes.string.isRequired,
+  imageThum: PropTypes.string.isRequired,
+  nameArt: PropTypes.string.isRequired,
+  nameAlb: PropTypes.string.isRequired,
+};
+export default Albuns;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Header from '../components/Header';
+import Carregando from './Carregando';
+import Albuns from '../components/Albuns';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
+      artist: '',
+      albunsArtist: [],
       habilitedBtn: true,
+      carregar: false,
+      carregarAlbum: false,
+      notAlbun: false,
     };
   }
 
@@ -14,10 +22,34 @@ class Search extends React.Component {
     if (target.value.length >= minNum) {
       this.setState({
         habilitedBtn: false,
+        artist: target.value,
       });
     } else {
       this.setState({
         habilitedBtn: true,
+      });
+    }
+  };
+
+  btnPesquisar = async (event) => {
+    const { state } = this;
+    event.preventDefault();
+    this.setState({
+      carregar: true,
+      albunsArtist: [],
+    });
+    document.getElementById('clearInput').value = '';
+    const apiArtirt = await searchAlbumsAPI(state.artist);
+    if (apiArtirt.length > 0) {
+      this.setState({
+        carregar: false,
+        carregarAlbum: true,
+        albunsArtist: apiArtirt,
+      });
+    } else {
+      this.setState({
+        notAlbun: true,
+        carregar: false,
       });
     }
   };
@@ -30,18 +62,37 @@ class Search extends React.Component {
         <form>
           <input
             data-testid="search-artist-input"
+            id="clearInput"
             type="text"
             onChange={ this.btnHabi }
           />
           <button
             data-testid="search-artist-button"
             type="submit"
+            onClick={ this.btnPesquisar }
             disabled={ state.habilitedBtn }
           >
             Pesquisar
 
           </button>
         </form>
+        {state.carregar && <Carregando />}
+        { state.notAlbun && <h2>Nenhum álbum foi encontrado</h2> }
+        {state.carregarAlbum && (
+          <h2>
+            Resultado de álbuns de:
+            {' '}
+            { state.artist }
+          </h2>)}
+        {state.carregarAlbum && state.albunsArtist.map((element) => (
+          <Albuns
+            key={ element.collectionId }
+            collectionId={ element.collectionId }
+            imageThum={ element.artworkUrl100 }
+            nameArt={ element.artistName }
+            nameAlb={ element.collectionName }
+          />
+        ))}
       </div>
     );
   }
